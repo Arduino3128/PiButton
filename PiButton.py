1205bc91991457734cbcc6eabda268568fe9256f
Kanad Nemade <kanadnemade88@gmail.com>
2020-05-09 22:32:44 +0530

    Update PiButton.py

--- PiButton.py
+++ PiButton.py
@@ -2,20 +2,18 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-state=GPIO.input(3)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+state=GPIO.input(17)
 x=0
 while x==0:
-    state=GPIO.input(3)
+    state=GPIO.input(17)
     if state==0:
         time.sleep(3)
-        state=GPIO.input(3)
+        state=GPIO.input(17)
         if state==0:
             x=1
             subprocess.call("reboot", shell=False)
         elif state==1:
             x=1
             subprocess.call(['shutdown', '-h', 'now'], shell=False)
-    #subprocess.call("reboot", shell=False)
-    #subprocess.call(['shutdown', '-h', 'now'], shell=False)
 
